bd5406b5c98b88dd0786d18d72d2630e9125f110
mbu93 <skorp42@web.de>
2023-01-28 16:12:19 +0100

    Add logic to freeze the stone

--- Assets/Scripts/CollusionWithTimer.cs
+++ Assets/Scripts/CollusionWithTimer.cs
@@ -7,8 +7,7 @@ public class CollusionWithTimer : MonoBehaviour
 {
   public int turningPoint;
   public int turningAreaSize;
-  public int footOfHill;
-  public int menuTreshold; 
+  public int timeout;
 
   private CircleCollider2D collider;
   private Rigidbody2D body;
@@ -16,7 +15,6 @@ public class CollusionWithTimer : MonoBehaviour
   private PolygonCollider2D playerCollider;
   private Rigidbody2D playerBody;
   private bool collidesWithPlayer;
-  private int collisionCounter; 
   private bool menuUntriggered;
   
 
@@ -26,24 +24,11 @@ public class CollusionWithTimer : MonoBehaviour
     player = GameObject.FindWithTag("Player");
     playerCollider = player.GetComponent<PolygonCollider2D>();
     playerBody = player.GetComponent<Rigidbody2D>();
-    menuTreshold = Random.Range(1, 5);
-    collisionCounter = 0;
     menuUntriggered = true;
   }
 
   private bool IsBehindTurningPoint() {
-    bool isBehind = this.body.position.x > turningPoint;
-    if (isBehind) {
-      this.collisionCounter += 1;
-      if (this.collisionCounter > this.menuTreshold) {
-        this.collisionCounter = 0;
-      } 
-    };
-    return isBehind;
-  }
-
-  private bool returnedAfterRoll() {
-    return this.body.position.x < footOfHill;
+    return this.body.position.x > turningPoint;
   }
 
   private bool IsOutOfTurningArea() {
@@ -54,29 +39,23 @@ public class CollusionWithTimer : MonoBehaviour
     return playerBody.position.x < this.body.position.x - this.collider.radius / 2 - 1;
   }
 
-  private bool maxCollidesReached() {
-    return this.collisionCounter >= this.menuTreshold;
-  }
   private IEnumerator LoadMenu() {
-    yield return new WaitForSeconds(5);
+    if (this.timeout == 0) {
+        this.timeout = 5;
+    }
+    yield return new WaitForSeconds(this.timeout);
     SceneManager.LoadScene("Main Menu");
   }
 
   void Update() {
-    if (collidesWithPlayer && this.IsBehindTurningPoint()) {
-      collidesWithPlayer = false;
-    }
-    if (!collidesWithPlayer && this.IsOutOfTurningArea() && this.IsRightOfPlayer()) {
+    this.body.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+    Physics2D.IgnoreCollision(playerCollider, collider, true);
+    if (!collidesWithPlayer && !this.IsRightOfPlayer()) {
       collidesWithPlayer = true;
     }
-    if (this.returnedAfterRoll() && this.maxCollidesReached() && menuUntriggered) {
-      this.body.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
-      StartCoroutine(LoadMenu());
-      this.menuUntriggered = false;
-    };
-
-
-    Physics2D.IgnoreCollision(playerCollider, collider, !collidesWithPlayer);
-        
-    }
+    if (collidesWithPlayer && this.menuUntriggered) {
+        StartCoroutine(LoadMenu());
+        this.menuUntriggered = false;
+    }   
+  }
 }
