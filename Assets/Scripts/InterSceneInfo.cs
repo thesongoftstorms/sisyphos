c8e9735d7403d0f65ea219475d4b381a8650572d
mbu93 <skorp42@web.de>
2023-01-28 18:35:03 +0100

    Add remaining levels

---
+++ Assets/Scripts/InterSceneInfo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterSceneInfo : MonoBehaviour {
+    public static InterSceneInfo Instance;
+    public int healthBar;
+
+    private void Awake() {    
+    // start of new code
+    if (Instance != null) {
+        Destroy(gameObject);
+        return;
+    }
+    // end of new code
+        Instance = this;
+        healthBar = 5;
+        DontDestroyOnLoad(gameObject);
+    }
+}
